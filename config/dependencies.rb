476691ef16ffda50347d1bafe5443c5f75c483de
John Schult <john@schult.us>
2009-01-24 14:44:51 -0500

    added merb-haml

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -14,6 +14,7 @@ dependency("merb-cache", merb_gems_version) do
 end
 dependency "merb-helpers", merb_gems_version 
 dependency "merb-mailer", merb_gems_version  
+dependency "merb-haml", merb_gems_version  
 dependency "merb-slices", merb_gems_version  
 dependency "merb-auth-core", merb_gems_version
 dependency "merb-auth-more", merb_gems_version
